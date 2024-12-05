766d77ab0b630616624478fa9a03c6cf83aaf587
flamendless <flamendless8@gmail.com>
2024-12-05 17:20:08 +0800

    Add templ in deps

--- run.sh
+++ run.sh
@@ -86,6 +86,7 @@ cleandb() {
 }
 
 deps() {
+	go install github.com/a-h/templ/cmd/templ@latest
 	go install github.com/rubenv/sql-migrate/...@latest
 	go install github.com/air-verse/air@latest
 	go install github.com/alexkohler/prealloc@latest
