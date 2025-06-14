a76a49f70b94cf666e170eab486a27288f9ef90b
flamendless <flamendless8@gmail.com>
2025-06-14 11:53:06 +0800

    Server: Implement prefix in base64 encoding

--- internal/serialize/dbid.go
+++ internal/serialize/dbid.go
@@ -1,30 +1,53 @@
 package serialize
 
 import (
+	"bytes"
+	"cchoice/internal/enums"
 	"encoding/base64"
-	"encoding/binary"
 	"fmt"
-	"strings"
+	"strconv"
 )
 
-//TODO: (Brandon) - implement prefix + id for safety
-
-const PADDING = "="
-
-func EncDBID(dbid int64) string {
-	idb := make([]byte, 8)
-	binary.LittleEndian.PutUint64(idb, uint64(dbid))
-	id := base64.URLEncoding.EncodeToString(idb)
-	id = strings.TrimSuffix(id, PADDING)
-	return id
+func EncodeDBID(prefix enums.DBPrefix, dbid int64) string {
+	var buf bytes.Buffer
+	buf.WriteString(prefix.String())
+	buf.WriteByte(':')
+	buf.WriteString(strconv.FormatInt(dbid, 10))
+	return base64.RawURLEncoding.EncodeToString(buf.Bytes())
 }
 
-func DecDBID(dbid string) int64 {
-	dbid += PADDING
-	decid, err := base64.URLEncoding.DecodeString(dbid)
+func DecodeToDBID(dbid string) (enums.DBPrefix, int64) {
+	dec, err := base64.RawURLEncoding.DecodeString(dbid)
 	if err != nil {
-		panic(fmt.Sprintf("Failed to decode string: '%s' %s", dbid, err.Error()))
+		panic(fmt.Errorf("Failed to decode string: '%s' %w", dec, err))
 	}
-	id := int64(binary.LittleEndian.Uint64(decid))
-	return id
+
+	splitted := bytes.Split(dec, []byte(":"))
+	prefix, decid := splitted[0], splitted[1]
+
+	id, err := strconv.Atoi(string(decid))
+	if err != nil {
+		panic(fmt.Errorf("Failed to decode string: '%s' %w", decid, err))
+	}
+
+	return enums.ParseDBPrefixToEnum(string(prefix)), int64(id)
+}
+
+func MustDecodeToDBID(prefix enums.DBPrefix, dbid string) int64 {
+	dec, err := base64.RawURLEncoding.DecodeString(dbid)
+	if err != nil {
+		panic(fmt.Errorf("Failed to decode string '%s' from '%s' %w", dec, dbid, err))
+	}
+
+	splitted := bytes.Split(dec, []byte(":"))
+	if string(splitted[0]) != prefix.String() {
+		panic(fmt.Errorf("Prefix must match: '%s' %s", prefix.String(), splitted[0]))
+	}
+
+	id, err := strconv.Atoi(string(splitted[1]))
+	if err != nil {
+		panic(fmt.Errorf("Failed to decode string: '%s' %w", splitted[1], err))
+	}
+
+	return int64(id)
 }
--- internal/serialize/dbid_test.go
+++ internal/serialize/dbid_test.go
@@ -1,6 +1,7 @@
 package serialize
 
 import (
+	"cchoice/internal/enums"
 	"math/rand"
 	"testing"
 	"time"
@@ -9,18 +10,31 @@ import (
 )
 
 func TestEncDecDBID(t *testing.T) {
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	test := int64(r.Uint64())
-	enc := EncDBID(test)
-	dec := DecDBID(enc)
-	require.Equal(t, test, dec)
+	enc := EncodeDBID(enums.DB_PREFIX_CATEGORY, int64(123))
+	prefix, dec := DecodeToDBID(enc)
+	require.Equal(t, enums.DB_PREFIX_CATEGORY, prefix)
+	require.Equal(t, int64(123), dec)
+	require.NotPanics(t, func() {
+		_ = MustDecodeToDBID(enums.DB_PREFIX_CATEGORY, enc)
+	})
+	require.Panics(t, func() {
+		_ = MustDecodeToDBID(enums.DB_PREFIX_UNDEFINED, enc)
+	})
+}
+
+func TestValidEncDecDBID(t *testing.T) {
+	const encID = "Q0FURUdPUlk6MTIz"
+	enc := EncodeDBID(enums.DB_PREFIX_CATEGORY, int64(123))
+	require.Equal(t, encID, enc)
+	dbid := MustDecodeToDBID(enums.DB_PREFIX_CATEGORY, encID)
+	require.Equal(t, int64(123), dbid)
 }
 
 func BenchmarkEncDecDBID(b *testing.B) {
 	for b.Loop() {
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
 		test := int64(r.Uint64())
-		enc := EncDBID(test)
-		_ = DecDBID(enc)
+		enc := EncodeDBID(enums.DB_PREFIX_CATEGORY, test)
+		_, _ = DecodeToDBID(enc)
 	}
 }
