cda725a95376a83331487b353adade6dbb798342
flamendless <flamendless8@gmail.com>
2025-06-27 23:52:05 +0800

    Script: Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+SSH_ADDR="${SSH_ADDR:-}"
+if [ -z "$SSH_ADDR" ]; then
+	echo "SSH_ADDR is not set."
+	exit 1
+fi
+
+ssh "$SSH_ADDR" bash --login -i -s <<EOF
+	set -euf -o pipefail
+
+	echo "Navigating to project directory..."
+	cd "cchoice"
+
+	echo "Syncing repository..."
+	git pull origin main
+
+	echo "Stopping existing process..."
+	pkill -f "./tmp/main api" || echo "No existing process found."
+
+	echo "Building..."
+	./run.sh prod
+
+	echo "Running API..."
+	./tmp/main api > out 2>&1 &
+
+	echo "Done!"
+EOF
