2a54a9adc23de17b019074e652b8975545aa9952
flamendless <flamendless@gmail.com>
2025-07-20 23:35:01 +0800

    Script: CCS-25 Add cmd for encoding/decoding

---
+++ cmd/encode.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"cchoice/internal/encode"
+	"cchoice/internal/encode/sqids"
+	"cchoice/internal/errs"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var flagIntID int64
+var flagStrID string
+var flagMethod string
+
+func init() {
+	f := cmdEncode.Flags
+	f().Int64VarP(&flagIntID, "int_id", "i", 0, "Database ID (int)")
+	f().StringVarP(&flagStrID, "str_id", "s", "", "Database ID (string)")
+	f().StringVarP(&flagMethod, "method", "x", "sqids", "Method (sqids)")
+
+	rootCmd.AddCommand(cmdEncode)
+}
+
+var cmdEncode = &cobra.Command{
+	Use:   "encode",
+	Short: "encode",
+	Run: func(cmd *cobra.Command, args []string) {
+		var encode encode.IEncode
+		switch flagMethod {
+		case "sqids":
+			encode = sqids.MustSqids()
+		default:
+			panic(errs.ERR_CMD)
+		}
+
+		if flagIntID != 0 {
+			fmt.Printf("%s\n", encode.Encode(flagIntID))
+		}
+
+		if flagStrID != "" {
+			fmt.Printf("%d\n", encode.Decode(flagStrID))
+		}
+	},
+}
