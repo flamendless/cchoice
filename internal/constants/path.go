c37cba28a081555f5dba52ba9154f76e8078ec08
flamendless <flamendless@gmail.com>
2025-08-04 21:47:04 +0800

    Bugfix: CCS-39 Exclude products with no valid image in home page

--- internal/constants/path.go
+++ internal/constants/path.go
@@ -1,6 +1,7 @@
 package constants
 
 const (
-	PathEmptyImage    = "static/images/empty_96x96.webp"
-	PathProductImages = "static/images/product_images/"
+	EmptyImageFilename = "empty_96x96.webp"
+	PathEmptyImage     = "static/images/empty_96x96.webp"
+	PathProductImages  = "static/images/product_images/"
 )
