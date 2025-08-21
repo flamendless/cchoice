f2dea68896d61866a351bb15189cca760bcc2258
flamendless <flamendless@gmail.com>
2025-08-21 12:39:04 +0800

    Maintenance: Apply sc

--- cmd/web/models/checkouts.go
+++ cmd/web/models/checkouts.go
@@ -3,15 +3,15 @@ package models
 import "github.com/Rhymond/go-money"
 
 type CheckoutLine struct {
+	Price           money.Money
+	DiscountedPrice money.Money
+	Total           money.Money
 	ID              string
 	CheckoutID      string
 	ProductID       string
 	Name            string
-	Price           money.Money
-	DiscountedPrice money.Money
-	Total           money.Money
 	BrandName       string
-	Quantity        int64
 	ThumbnailPath   string
 	ThumbnailData   string
+	Quantity        int64
 }
