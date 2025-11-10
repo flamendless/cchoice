3af6332969888541e80a704038289427d67f67f1
flamendless <flamendless@gmail.com>
2025-11-10 16:36:43 +0800

    Maintenance: Update cmd and add testInteg

--- scripts/pre-commit-unit-test.sh
+++ scripts/pre-commit-unit-test.sh
@@ -3,8 +3,9 @@
 echo "Running unit tests..."
 
 mage testAll
+mage testInteg
 
 if [ $? -ne 0 ]; then
-	echo "You have an error with unit tests. Run 'mage testAll'"
+	echo "You have an error with unit tests. Run `mage testAll` and `mage testInteg`"
 	exit 1
 fi
