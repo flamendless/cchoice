6d5222f4b021568f5c095479e23327006d92bd10
flamendless <flamendless@gmail.com>
2025-12-18 00:08:27 +0800

    Server: Implement changelogs parsing

--- internal/constants/path.go
+++ internal/constants/path.go
@@ -7,4 +7,5 @@ const (
 	PathPaymentImages  = "static/images/payments/"
 	PathEmailLogoCDN   = "https://imagedelivery.net/YnES7emCTPeSEVA2N0dB_g/c89c033a-ebd3-4eed-0b79-09bfbcd16800/public"
 	PathEmptyImageCDN  = "https://imagedelivery.net/YnES7emCTPeSEVA2N0dB_g/empty_96x96/public"
+	PathChangelogs     = "./CHANGELOGS.md"
 )
