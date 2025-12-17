abb3ae3a6562f93f4a16c17e43b5d4c9f2d16d47
flamendless <flamendless@gmail.com>
2025-12-18 00:08:49 +0800

    Web: Improve changelogs view

--- cmd/web.go
+++ cmd/web.go
@@ -18,18 +18,11 @@ var cmdWeb = &cobra.Command{
 	Short: "Run the web frontend only (no payment/shipping/mail services)",
 	Run: func(cmd *cobra.Command, args []string) {
 		serverInstance := server.NewServer()
-		done := make(chan bool, 1)
-
-		go gracefulShutdown(serverInstance, done)
-
 		httpServer := serverInstance.HTTPServer
 
 		logs.Log().Info("Serving HTTP (web mode)")
 		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			panic(fmt.Sprintf("http server error: %s", err))
 		}
-
-		<-done
-		logs.Log().Info("Graceful shutdown complete.")
 	},
 }
