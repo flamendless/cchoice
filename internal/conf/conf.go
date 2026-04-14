2db4dcd8581fb1c806d9d89b71ed4ab8f5bf973c
flamendless <flamendless8@gmail.com>
2026-04-14 08:23:01 +0800

    Server: Add LocalForgotPassword testing env var

--- internal/conf/conf.go
+++ internal/conf/conf.go
@@ -50,8 +50,9 @@ type appConfig struct {
 }
 
 type Test struct {
-	LocalUploadImage bool `env:"TEST_LOCAL_UPLOAD_IMAGE" env-default:"0"`
-	LocalOTP         bool `env:"TEST_LOCAL_OTP" env-default:"0"`
+	LocalUploadImage    bool `env:"TEST_LOCAL_UPLOAD_IMAGE" env-default:"0"`
+	LocalOTP            bool `env:"TEST_LOCAL_OTP" env-default:"0"`
+	LocalForgotPassword bool `env:"TEST_LOCAL_FORGOT_PASSWORD" env-default:"0"`
 }
 
 type RateLimitConfig struct {
