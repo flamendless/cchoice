a1151882aa0e21b5be2e326781a4865fcb5677d3
arjcrentoria <arjcrentoria@gmail.com>
2026-06-30 08:26:25 +0800

    Feature: add image carousel to Our Store section

---
+++ internal/constants/store_images.go
@@ -0,0 +1,12 @@
+package constants
+func GetStoreImages() []string {
+	return []string{
+		"https://imagedelivery.net/YnES7emCTPeSEVA2N0dB_g/120dbd5d-37ff-4e7b-f377-86599cb17c00/public",
+		"https://imagedelivery.net/YnES7emCTPeSEVA2N0dB_g/da39a0aa-aa11-4cd4-6796-5e4093816300/public",
+		"https://imagedelivery.net/YnES7emCTPeSEVA2N0dB_g/39e491f2-3455-4cb2-acf5-5c57ed468200/public",
+		"https://imagedelivery.net/YnES7emCTPeSEVA2N0dB_g/09f1e2e3-51aa-488a-4d9f-5bcb61364200/public",
+		"https://imagedelivery.net/YnES7emCTPeSEVA2N0dB_g/737e5166-c31a-4b8f-6153-6c64b15dbc00/public",
+		"https://imagedelivery.net/YnES7emCTPeSEVA2N0dB_g/f5d9bc5a-b56f-4ddd-7b11-fe24f2fe0900/public",
+		"https://imagedelivery.net/YnES7emCTPeSEVA2N0dB_g/9963f3d3-b236-4e76-e614-4ed10a75e500/public",
+	}
+}
