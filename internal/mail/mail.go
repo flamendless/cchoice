e3231477c1cc794121a486335a3e65e5b474d6eb
flamendless <flamendless8@gmail.com>
2026-08-02 12:04:08 +0000

    Feature: Support email attachments in mail service

--- internal/mail/mail.go
+++ internal/mail/mail.go
@@ -10,8 +10,15 @@ type EmailMessage struct {
 
 type TemplateData map[string]any
 
+type Attachment struct {
+	FileName    string
+	ContentType string
+	Content     []byte
+}
+
 type IMailService interface {
 	Enum() MailService
 	SendEmail(to string, cc []string, subject, body string) error
 	SendTemplateEmail(to string, cc []string, subject, templateName string, data TemplateData) error
+	SendTemplateEmailWithAttachments(to string, cc []string, subject, templateName string, data TemplateData, attachments []Attachment) error
 }
